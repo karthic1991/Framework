f8632d9c7c7daa7ff70617dcb32b173280ee5a0a
karthick1991 <gkmech1991@gmail.com>
2025-04-06 17:45:52 +0530

    first commit

---
+++ src/test/java/project/Framework/StandAloneProject.java
@@ -0,0 +1,23 @@
+package project.Framework;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneProject {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+		driver.get("https://rahulshettyacademy.com/client");
+		
+		
+
+	}
+
+}
