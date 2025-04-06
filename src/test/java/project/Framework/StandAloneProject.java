f9a9da267d3edb04d2bcc84050c59cb280a16a28
karthick1991 <gkmech1991@gmail.com>
2025-04-06 18:45:23 +0530

    test change

--- src/test/java/project/Framework/StandAloneProject.java
+++ src/test/java/project/Framework/StandAloneProject.java
@@ -2,6 +2,7 @@ package project.Framework;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,6 +16,10 @@ public class StandAloneProject {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
 		driver.get("https://rahulshettyacademy.com/client");
+		
+		driver.findElement(By.id("userEmail")).sendKeys("gkmech1991@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Afcat@1991");
+		driver.findElement(By.id("login")).click(); 
 		
 		
 
